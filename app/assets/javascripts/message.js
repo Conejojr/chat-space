152c5876fda83b7fa4930d53faafb1dc79736b85
kiyoshi <kiyo7s@icloud.com>
2019-09-01 20:35:52 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -51,30 +51,32 @@ $(function(){
       alert('メッセージの送信に失敗しました');
     })
   })
-
-  var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    last_message_id = $('.contents__chat__list:last').data('id')
-    $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      console.log(messages)
-      messages.forEach(function(message){
-        var html = buildHTML(message);
-        $('.contents__chat').append(html)
+  var url = location.pathname;
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $('.contents__chat__list:last').data('id')
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        console.log(messages)
+        messages.forEach(function(message){
+          var html = buildHTML(message);
+          $('.contents__chat').append(html)
+        });
+        $(".contents__chat").scrollTop( $(".contents__chat").get(0).scrollHeight )
+      })
+      .fail(function() {
+        console.log('error');
       });
-      $(".contents__chat").scrollTop( $(".contents__chat").get(0).scrollHeight )
-    })
-    .fail(function() {
-      console.log('error');
-    });
-  };
+    };
+  }
   setInterval(reloadMessages, 5000);
 });
